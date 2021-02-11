4f69e5f5d697e698573f1045ad4c0904601d2094
kumar <kumareddy14322gmail.com>
2021-02-11 12:46:48 +0530

    modified files

---
+++ grpc2/grpcClient/main.go
@@ -0,0 +1,33 @@
+package main
+import (
+	"log"
+	 pb "c:Users/Kumar/Desktop/simple_cp_client_server/simple_tcp_client_server/grpc2/datafiles/datafiles"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+address = "localhost:50051"
+)
+
+func main() {
+// Set up a connection to the server.
+conn, err := grpc.Dial(address, grpc.WithInsecure())
+if err != nil {
+	log.Fatalf("Did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewMoneyTransactionClient(conn)
+	// Prepare data. Get this from clients like Frontend or App
+	from := "1234"
+	to := "5678"
+	amount := float32(1250.75)
+	// Contact the server and print out its response.
+	r, err := c.MakeTransaction(context.Background(),
+	&pb.TransactionRequest{From: from,
+	To: to, Amount: amount})
+	if err != nil {
+	log.Fatalf("Could not transact: %v", err)
+	}
+	log.Printf("Transaction confirmed: %t", r.Confirmation)
+}
